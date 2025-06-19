a945ff781c77192e97a7f67a5448e782742a08a0
cattiste <bruno.cattiste@gmail.com>
2025-06-19 19:46:16 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 export default function Login() {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Login</h1>
-      <p>Faça login para acessar sua conta.</p>
+    <div>
+      <h1>Página de Login</h1>
+      <button>Entrar</button>
     </div>
   )
 }
