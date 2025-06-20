6dfbe57247f8aaa59062fbc89dd758fdccaf5d55
cattiste <bruno.cattiste@gmail.com>
2025-06-20 14:06:49 -0300

    Update Contratar.jsx

--- src/pages/Contratar.jsx
+++ src/pages/Contratar.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import './Contratar.css'
+import { profissionais } from '../data/profissionais'
 
 const profissionais = [
   {
