40626d7fd2e22415ef13c763a6e3148be88a1306
cattiste <bruno.cattiste@gmail.com>
2025-06-20 14:17:40 -0300

    Create FiltroForm.jsx

---
+++ src/components/FiltroForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function FiltroForm({
+  filtroEspecialidade,
+  setFiltroEspecialidade,
+  filtroCidade,
+  setFiltroCidade,
+  filtroDisponibilidade,
+  setFiltroDisponibilidade
+}) {
+  return (
+    <div className="filtros-container">
+      <input
+        type="text"
+        placeholder="Especialidade (ex: Sushi, Churrasco...)"
+        value={filtroEspecialidade}
+        onChange={e => setFiltroEspecialidade(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Cidade"
+        value={filtroCidade}
+        onChange={e => setFiltroCidade(e.target.value)}
+      />
+      <select
+        value={filtroDisponibilidade}
+        onChange={e => setFiltroDisponibilidade(e.target.value)}
+      >
+        <option value="">Disponibilidade</option>
+        <option>Hoje</option>
+        <option>Esta semana</option>
+        <option>Este mês</option>
+      </select>
+    </div>
+  )
+}
