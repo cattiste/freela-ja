f7ad6d83811c7177ddf583e1629aeda38a44d8a3
cattiste <bruno.cattiste@gmail.com>
2025-06-21 17:05:02 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import App from './App'
+import './index.css'  // <- Isso ativa o Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
