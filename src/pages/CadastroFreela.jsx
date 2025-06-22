1dbfd898ea858ddc512bd20e3f44c8b84e3fa021
cattiste <bruno.cattiste@gmail.com>
2025-06-22 17:40:09 -0300

    d

--- src/pages/CadastroFreela.jsx
+++ src/pages/CadastroFreela.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import './CadastroFreela.css'
 
 export default function CadastroFreela() {
   const navigate = useNavigate()
