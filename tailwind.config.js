57180ae303a26afb8c8674b2a156253245b6832d
cattiste <bruno.cattiste@gmail.com>
2025-06-25 22:40:14 -0300

    revisados 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    // Remova os requires e use apenas os nomes dos plugins
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
