70c202dc32ccb308e4fab175fd46a6d2d6d229ae
cattiste <bruno.cattiste@gmail.com>
2025-06-27 17:49:22 -0300

    painel

--- src/pages/PainelEstabelecimento.jsx
+++ src/pages/PainelEstabelecimento.jsx
@@ -3,10 +3,10 @@ import { auth, db } from '@/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import { getDoc, doc } from 'firebase/firestore'
 
-import BuscarFreelas from './BuscarFreelas'
-import ChamadasEstabelecimento from './ChamadasEstabelecimento'
-import AgendasContratadas from './AgendasContratadas'
-import AvaliacaoFreela from './AvaliacaoFreela'
+import BuscarFreelas from '@/BuscarFreelas'
+import ChamadasEstabelecimento from '@/ChamadasEstabelecimento'
+import AgendasContratadas from '@/AgendasContratadas'
+import AvaliacaoFreela from '@/AvaliacaoFreela'
 
 export default function PainelEstabelecimento() {
   const [aba, setAba] = useState('buscar')
