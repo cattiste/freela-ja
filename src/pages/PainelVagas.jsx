8549a19c272b04e2033fa42d8eb5940dcaddbe99
cattiste <bruno.cattiste@gmail.com>
2025-06-27 19:35:36 -0300

    corrigido

---
+++ src/pages/PainelVagas.jsx
@@ -0,0 +1,11 @@
+// src/pages/PainelVagas.jsx
+import React from 'react'
+
+export default function PainelVagas() {
+  return (
+    <div>
+      <h1>Painel de Vagas</h1>
+      <p>Aqui você poderá gerenciar as vagas.</p>
+    </div>
+  )
+}
