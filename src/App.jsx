f287ff5b76f54e525d3ed1f38d0a33df920bd951
cattiste <bruno.cattiste@gmail.com>
2025-06-27 15:20:26 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Sobre from './pages/Sobre'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
 import PainelFreela from './pages/PainelFreela'
-import PainelEstabelecimento from './pages/PainelEstabelecimento' // veja pasta estabelecimento
 import Curriculos from './pages/Curriculos'
 import Perfil from './pages/Perfil'
 import CadastroFreela from './pages/CadastroFreela'
@@ -18,7 +17,6 @@ import EditarFreela from './pages/EditarFreela'
 import RotaProtegidaFreela from './components/RotaProtegidaFreela'
 import RotaProtegidaEstabelecimento from './components/RotaProtegidaEstabelecimento'
 import EsqueciSenha from './pages/EsqueciSenha'
-import EditarPerfilEstabelecimento from './pages/estabelecimento/EditarPerfilEstabelecimento'
 
 export default function App() {
   return (
@@ -38,7 +36,7 @@ export default function App() {
           <Route path="/cadastro-estabelecimento" element={<CadastroEstabelecimento />} />
           <Route path="/perfil/:id" element={<Perfil />} />
           <Route path="/editarfreela/:id" element={<EditarFreela />} />
-          <Route path="/editarperfilestabelecimento" element={<EditarPerfilEstabelecimento />} />
+          
 
           {/* Painéis protegidos */}
           <Route
@@ -49,15 +47,7 @@ export default function App() {
               </RotaProtegidaFreela>
             }
           />
-          <Route
-            path="/painel-estabelecimento"
-            element={
-              <RotaProtegidaEstabelecimento>
-                <PainelEstabelecimento />
-              </RotaProtegidaEstabelecimento>
-            }
-          />
-
+          
           {/* Vagas */}
           <Route path="/vagas" element={<PainelVagas />} />
           <Route path="/publicarvaga" element={<PublicarVaga />} />
