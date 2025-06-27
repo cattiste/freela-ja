f8ee6fded2d6f7eebbc360f2855b6f19acad4d75
cattiste <bruno.cattiste@gmail.com>
2025-06-27 17:31:23 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Sobre from './pages/Sobre'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
 import PainelFreela from './pages/PainelFreela'
+import PainelEstabelecimento from './pages/PainelEstabelecimento' // veja pasta estabelecimento
 import Curriculos from './pages/Curriculos'
 import Perfil from './pages/Perfil'
 import CadastroFreela from './pages/CadastroFreela'
@@ -17,7 +18,7 @@ import EditarFreela from './pages/EditarFreela'
 import RotaProtegidaFreela from './components/RotaProtegidaFreela'
 import RotaProtegidaEstabelecimento from './components/RotaProtegidaEstabelecimento'
 import EsqueciSenha from './pages/EsqueciSenha'
-import PainelEstabelecimento from './pages/PainelEstabelecimento';
+import EditarPerfilEstabelecimento from './pages/estabelecimento/EditarPerfilEstabelecimento'
 
 export default function App() {
   return (
@@ -37,7 +38,7 @@ export default function App() {
           <Route path="/cadastro-estabelecimento" element={<CadastroEstabelecimento />} />
           <Route path="/perfil/:id" element={<Perfil />} />
           <Route path="/editarfreela/:id" element={<EditarFreela />} />
-          <Route path="/painel-estabelecimento" element={<PainelEstabelecimento />}/>
+          <Route path="/editarperfilestabelecimento" element={<EditarPerfilEstabelecimento />} />
 
           {/* Painéis protegidos */}
           <Route
@@ -48,7 +49,15 @@ export default function App() {
               </RotaProtegidaFreela>
             }
           />
-          
+          <Route
+            path="/painel-estabelecimento"
+            element={
+              <RotaProtegidaEstabelecimento>
+                <PainelEstabelecimento />
+              </RotaProtegidaEstabelecimento>
+            }
+          />
+
           {/* Vagas */}
           <Route path="/vagas" element={<PainelVagas />} />
           <Route path="/publicarvaga" element={<PublicarVaga />} />
