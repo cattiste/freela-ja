39461ebc573d138caccb9f83adb051db8dfcf693
cattiste <bruno.cattiste@gmail.com>
2025-06-27 17:52:33 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -6,10 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Permite usar ''@/firebase', '@/pages', etc.
-    },
-  },
-  build: {
-    outDir: 'dist' // Mantém seu destino de build
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
