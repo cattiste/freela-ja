3ce55e642917ef1c9092e3bbeabec5693dc33976
cattiste <bruno.cattiste@gmail.com>
2025-06-27 17:32:26 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./index.html",       // ajuste esses caminhos conforme seu projeto
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Permite usar ''@/firebase', '@/pages', etc.
+    },
+  },
+  build: {
+    outDir: 'dist' // Mantém seu destino de build
   }
 })
