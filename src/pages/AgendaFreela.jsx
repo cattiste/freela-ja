369fd6b991312048fb42e3ba1a1e35c899835811
cattiste <bruno.cattiste@gmail.com>
2025-07-02 22:52:22 -0300

    agenda

--- src/pages/AgendaFreela.jsx
+++ src/pages/AgendaFreela.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Calendar from 'react-calendar'
-import 'react-calendar/styles/Calendar.css'
+import '../styles/calendar.css'
 import { db } from '@/firebase'
 import {
   collection,
