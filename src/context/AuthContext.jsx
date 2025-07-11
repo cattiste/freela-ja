dbf20b6c781d590ac85fe6ecbf96b06ea823241c
cattiste <bruno.cattiste@gmail.com>
2025-07-11 18:39:34 -0300

    1

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+// src/context/AuthContext.jsx
 import { createContext, useContext, useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/firebase'
@@ -8,22 +9,24 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, async (usuario) => {
-    if (usuario) {
-      // Buscar dados adicionais no Firestore
-      const ref = doc(db, 'usuarios', usuario.uid)
-      const snap = await getDoc(ref)
-      const dataExtra = snap.exists() ? snap.data() : {}
+    const unsubscribe = onAuthStateChanged(auth, (usuario) => {
+      if (usuario) {
+        setUser(usuario)
+      } else {
+        setUser(null)
+      }
+    })
 
-      setUser({
-        uid: usuario.uid,
-        email: usuario.email,
-        ...dataExtra
-      })
-    } else {
-      setUser(null)
-    }
-  })
+    return () => unsubscribe()
+  }, [])
 
-  return () => unsubscribe()
-}, [])
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
