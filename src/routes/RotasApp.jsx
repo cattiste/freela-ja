e794a22f376eedab87539874b2422da2a25fb0fa
cattiste <bruno.cattiste@gmail.com>
2025-07-14 18:01:26 -0300

    1

---
+++ src/routes/RotasApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+import RotasPublicas from './RotasPublicas'
+import { RotasFreela } from './RotasFreela'
+import { RotasEstabelecimento } from './RotasEstabelecimento'
+
+export default function RotasApp({ usuario }) {
+  return (
+    <Routes>
+      <RotasPublicas />
+      <RotasFreela usuario={usuario} />
+      <RotasEstabelecimento usuario={usuario} />
+    </Routes>
+  )
+}
