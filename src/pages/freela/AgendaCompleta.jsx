e30d72dd1131e3c5bf29555f1830665f45408c7b
cattiste <bruno.cattiste@gmail.com>
2025-07-20 13:34:22 -0300

    1

--- src/pages/freela/AgendaCompleta.jsx
+++ src/pages/freela/AgendaCompleta.jsx
@@ -1,5 +1,4 @@
 // src/pages/freela/AgendaCompleta.jsx
-
 import React from 'react'
 import AgendaFreela from './AgendaFreela'
 import VagasDisponiveis from './VagasDisponiveis'
@@ -8,12 +7,9 @@ import EventosDisponiveis from './EventosDisponiveis'
 export default function AgendaCompleta({ freelaId }) {
   return (
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-6">
-      {/* Calendário */}
       <div>
         <AgendaFreela freelaId={freelaId} />
       </div>
-
-      {/* Vagas e Eventos lado a lado */}
       <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-6">
         <VagasDisponiveis freelaId={freelaId} />
         <EventosDisponiveis freelaId={freelaId} />
