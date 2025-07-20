d8e401f62d372dbe91986d70ec38748622ce40a7
cattiste <bruno.cattiste@gmail.com>
2025-07-20 13:30:56 -0300

    1

--- src/pages/freela/AgendaCompleta.jsx
+++ src/pages/freela/AgendaCompleta.jsx
@@ -1,11 +1,11 @@
+// src/pages/freela/AgendaCompleta.jsx
+
 import React from 'react'
 import AgendaFreela from './AgendaFreela'
 import VagasDisponiveis from './VagasDisponiveis'
 import EventosDisponiveis from './EventosDisponiveis'
 
-export default function AgendaCompleta({ freela }) {
-  const freelaId = freela?.uid
-
+export default function AgendaCompleta({ freelaId }) {
   return (
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-6">
       {/* Calendário */}
