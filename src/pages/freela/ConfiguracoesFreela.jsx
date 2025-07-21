ad3158869d451ef7db7532636984d0408c85c58d
cattiste <bruno.cattiste@gmail.com>
2025-07-20 21:11:20 -0300

    1

--- src/pages/freela/ConfiguracoesFreela.jsx
+++ src/pages/freela/ConfiguracoesFreela.jsx
@@ -139,7 +139,7 @@ export default function ConfiguracoesFreela() {
       {/* Ações */}
       <div className="flex flex-wrap gap-4 mt-4">
         <button
-          onClick={() => navigate('/freela/EditarFreela')}
+          onClick={() => navigate('/freela/editarfreela')}
           className="bg-blue-600 text-white px-4 py-2 rounded"
         >
           ✏️ Editar Perfil
