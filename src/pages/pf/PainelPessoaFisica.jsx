e0adb67104f05916998e5182865be0fa01e13c83
cattiste <bruno.cattiste@gmail.com>
2025-08-07 20:37:43 -0300

    1

--- src/pages/pf/PainelPessoaFisica.jsx
+++ src/pages/pf/PainelPessoaFisica.jsx
@@ -59,7 +59,7 @@ export default function PainelPessoaFisica() {
   const renderConteudo = () => {
     switch (abaSelecionada) {
       case 'buscar':
-        return <BuscarFreelas usuario={usuario} tipoChamador="pessoa_fisica" />;
+        return <BuscarFreelas estabelecimento={estabelecimento} usuariosOnline={usuariosOnline} />
       case 'candidaturas':
         return <ChamadasPessoaFisica usuario={usuario} />;
       case 'agenda':
