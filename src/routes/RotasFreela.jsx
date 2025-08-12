018d38714a4485bdb68602df1992ea30ec1f6bb9
cattiste <bruno.cattiste@gmail.com>
2025-08-12 15:52:14 -0300

    2

--- src/routes/RotasFreela.jsx
+++ src/routes/RotasFreela.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import RotaProtegidaFreela from '@/components/protegidas/RotaProtegidaFreela.jsx'
-import CadastroFreela from '@/pages/CadastroFreela'
-import EditarFreela from '@/pages/EditarFreela'
-import PainelFreela from '@/pages/PainelFreela'
-import PerfilFreela from '@/pages/PerfilFreela'
-import VagasDisponiveis from '@/pages/VagasDisponiveis'
-import PublicarEvento from '@/pages/PublicarEvento'
-import EventosDisponiveis from '@/pages/EventosDisponiveis'
-import CadastroEvento from '@/pages/CadastroEvento'
+import CadastroFreela from '@/pages/freela/CadastroFreela'
+import EditarFreela from '@/pages/freela/EditarFreela'
+import PainelFreela from '@/pages/freela/PainelFreela'
+import PerfilFreela from '@/pages/freela/PerfilFreela'
+import VagasDisponiveis from '@/pages/freela/VagasDisponiveis'
+import EventosDisponiveis from '@/pages/freela/EventosDisponiveis'
+
 
 console.log('🔁 Render RotasPublicas')
 
