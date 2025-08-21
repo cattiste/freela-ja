5c1c5f5cc4f6ba774598c29468d642af739a2396
cattiste <bruno.cattiste@gmail.com>
2025-08-21 18:36:15 -0300

    1

---
+++ src/pages/admin/PainelAdmin.jsx
@@ -0,0 +1,11 @@
+// src/pages/admin/PainelAdmin.jsx
+import React from 'react'
+import DashboardAdmin from '@/components/DashboardAdmin'
+
+export default function PainelAdmin() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <DashboardAdmin />
+    </div>
+  )
+}
