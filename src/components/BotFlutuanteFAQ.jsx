dbd29b291ada4945cd398d7beae294b38786741d
cattiste <bruno.cattiste@gmail.com>
2025-09-02 09:30:56 -0300

    1

--- src/components/BotFlutuanteFAQ.jsx
+++ src/components/BotFlutuanteFAQ.jsx
@@ -10,11 +10,7 @@ const perguntasRespostas = [
       {        
         pergunta: 'Quais são as taxas?',
         resposta:
-          'Transparência e Simplicidade
-Na nossa plataforma, cobramos apenas 10% de taxa de serviço sobre cada chamada realizada.
-Nenhuma outra taxa será aplicada — publicar vagas e se candidatar é sempre 100% gratuito.
-
-Essa contribuição é o que mantém a plataforma funcionando com qualidade, garantindo segurança, suporte e melhorias contínuas para todos os usuários.',
+          'Transparência e Simplicidade Na nossa plataforma, cobramos apenas 10% de taxa de serviço sobre cada chamada realizada. Nenhuma outra taxa será aplicada, publicar vagas e se candidatar é sempre 100% gratuito. Essa contribuição é o que mantém a plataforma funcionando com qualidade, garantindo segurança, suporte e melhorias contínuas para todos os usuários.'
       },
       {        
         pergunta: 'O que é check-in e check-out?',
