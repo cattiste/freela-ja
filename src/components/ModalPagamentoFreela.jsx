e4041e6b132e4d44afde5694fda02fcd89487aec
cattiste <bruno.cattiste@gmail.com>
2025-09-09 18:27:00 -0300

    3

--- src/components/ModalPagamentoFreela.jsx
+++ src/components/ModalPagamentoFreela.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { db } from "../firebaseConfig";
+import { db } from "../services/firebaseConfig";
 import { doc, onSnapshot } from "firebase/firestore";
 
 export default function ModalPagamentoFreela({ chamada, onClose }) {
