97cdb8f82075aa9808359885ef86e07687c72b34
cattiste <bruno.cattiste@gmail.com>
2025-09-14 22:16:52 -0300

    1

--- src/pages/freela/ChamadasFreela.jsx
+++ src/pages/freela/ChamadasFreela.jsx
@@ -214,7 +214,7 @@ function ChamadaItem({ ch }) {
               </button>
             )}
 
-            {statusEfetivo === 'pago' && (
+            {(statusEfetivo === 'aceita' || statusEfetivo === 'pago') && (
               <button
                 onClick={() => setMostrarModal(true)}
                 className="flex-1 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
